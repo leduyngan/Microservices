6d82aaa44c5416b61880b5aeff10127b76e4c997
DuyNgan <nganld1@runsystem.net>
2025-02-07 22:00:13 +0700

    2.2 CURD cho Product.API

---
+++ src/Services/Product.API/MappingProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+// using Infrastructure.Extensions;
+using Infrastructure.Mappings;
+using Product.API.Entities;
+using Shared.DTOs.Product;
+
+namespace Product.API;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CatalogProduct, ProductDto>();
+        CreateMap<CreateProductDto, CatalogProduct>();
+        CreateMap<UpdateProductDto, CatalogProduct>()
+        .IgnoreAllNonExisting();
+    }
+}
