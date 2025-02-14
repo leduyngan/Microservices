445cb3158032eae2b199d0a578a950b050d77b77
DuyNgan <nganld1@runsystem.net>
2025-02-14 16:21:38 +0700

    4.3 Repository basket api

---
+++ src/Services/Basket.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+using Contracts.Common.Interfaces;
+using Infrastructure.Common;
+
+namespace Basket.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services) =>
+        services.AddScoped<IBasketRepository, BasketRepository>()
+            .AddTransient<ISerializeService, SerializeService>();
+
+    public static void ConfigureRedis(this IServiceCollection services, IConfiguration configuration)
+    {
+        var redisConnnectionString = configuration.GetSection("CacheSetting:ConnectionString").Value;
+        if (string.IsNullOrEmpty(redisConnnectionString))
+        {
+            throw new ArgumentNullException("Redis connection string is not configured.");
+        }
+        
+        //Redis Configuration
+        services.AddStackExchangeRedisCache(options => options.Configuration = redisConnnectionString);
+    }
+}
--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -1,15 +1,20 @@
+using Basket.API.Extensions;
 using Common.Logging;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseSerilog(Serilogger.Configure);
 
-Log.Information("Starting Basket API up");
+Log.Information($"Start {builder.Environment.ApplicationName} up");
 
 try
 {
+    builder.Host.UseSerilog(Serilogger.Configure);
+    builder.Host.AddAppConfigurations();
+    builder.Host.AddAppConfigurations();
+    
     // Add services to the container.
-
+    builder.Services.ConfigureServices();
+    builder.Services.ConfigureRedis(builder.Configuration);
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
@@ -20,11 +25,15 @@ try
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
-        app.UseSwagger(); 
-        app.UseSwaggerUI();
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                $"{builder.Environment.ApplicationName} v1"));
+        });
     }
 
-    app.UseHttpsRedirection();
+    // app.UseHttpsRedirection(); //production only
 
     app.UseAuthorization();
 
@@ -32,12 +41,15 @@ try
 
     app.Run();
 }
-catch (Exception e)
+catch (Exception ex)
 {
-    Log.Fatal(e, "Unhandled exception");
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal)) throw;
+
+    Log.Fatal(ex, $"Unhandled exception: {ex.Message}");
 }
 finally
 {
-    Log.Information("Shut down Basket API copmlete");
+    Log.Information("Shut down Customer API copmlete");
     Log.CloseAndFlush();
 }
